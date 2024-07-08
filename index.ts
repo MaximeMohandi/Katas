5269063aae3a33810ece15b8c207e32b3c48794b
Maxime Mohandi <mohandi.maxime@gmail.com>
2024-07-08 21:59:57 +0200

    refactor(roman): 💡 move regex check into own method

--- index.ts
+++ index.ts
@@ -1,17 +1,23 @@
 const ROMAN_NUMERALS: { [key: string]: number } = { "I": 1, "V": 5, "X": 10, "D": 50, "C": 100, "M": 1000 };
 
 export function translateRomanNumber(romanNumber: string): number {
-        let standardizedRomanNumber = [...romanNumber.toUpperCase()];
+        let standardizedRomanNumber = romanNumber.toUpperCase();
 
-        let checkMoreThan2RepetitionPattern = new RegExp(`(.)\\1{${3},}`);
-        if (checkMoreThan2RepetitionPattern.test(romanNumber.toUpperCase())) {
+        if (hasNumeralRepeatedMoreThan3Time(romanNumber)) {
                 throw new Error('Numeral is repeated more than three time.');
         }
 
         let result = 0;
-        standardizedRomanNumber.map((numeral: string) => {
+        [...standardizedRomanNumber].map((numeral: string) => {
                 result += ROMAN_NUMERALS[numeral]
         });
 
         return result;
+}
+
+
+function hasNumeralRepeatedMoreThan3Time(romanNumber: string): boolean {
+        const checkIfMoreThan2ConsecutiveChar = `(.)\\1{${3},}`;
+
+        return new RegExp(checkIfMoreThan2ConsecutiveChar).test(romanNumber);
 }
