146912744da833158fa3b29ac69287074850ef4e
Maxime Mohandi <mohandi.maxime@gmail.com>
2024-07-08 20:03:57 +0200

    feat(roman): 🎸 translate 100 and 1000

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-const ROMAN_NUMERALS: { [key: string]: number } = { "I": 1, "V": 5, "X": 10, "D": 50 };
+const ROMAN_NUMERALS: { [key: string]: number } = { "I": 1, "V": 5, "X": 10, "D": 50, "C": 100, "M": 1000 };
 
 export function translateRomanNumber(romanNumber: string): number {
 
--- part1.test.ts
+++ part1.test.ts
@@ -6,9 +6,9 @@
 import { translateRomanNumber } from '.'
 
 describe('should read the 7 roman numerals', () => {
-        const cases = [['I', 1], ['V', 5], ['X', 10], ['D', 50]];
+        const cases = [['I', 1], ['V', 5], ['X', 10], ['D', 50], ['C', 100], ['M', 1000]];
 
-        test.each(cases)('Should translate %s to %d',
+        test.each(cases)('Should translate %p to %p',
                 (romanNumeralToTranslate, expectedDecimal) => {
                         expect(translateRomanNumber(romanNumeralToTranslate as string)).toBe(expectedDecimal);
                 }
