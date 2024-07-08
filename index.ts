4b227dc4302b33337fd9e5513aaf7aeb3a2bca09
Maxime Mohandi <mohandi.maxime@gmail.com>
2024-07-08 16:11:13 +0200

    refactor(roman): 💡 indentation

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 export function translateRomanNumber(romanNumber: string) {
-	if(romanNumber === "I") return 1;
+        if (romanNumber === "I") return 1;
 
-	if(romanNumber === "V") return 5;
+        if (romanNumber === "V") return 5;
 
-	if(romanNumber === "X") return 10;
+        if (romanNumber === "X") return 10;
 
-	if(romanNumber === "D") return 50;
+        if (romanNumber === "D") return 50;
 }
--- part1.test.ts
+++ part1.test.ts
@@ -3,20 +3,20 @@
  * should convert all 7 letteres to correct number (I:1, V:5, X:10, D:50, C:100, M:1000)
  * 
  */
-import {translateRomanNumber} from '.'
+import { translateRomanNumber } from '.'
 
-test('should convert I into 1',() => {
-	expect(translateRomanNumber('I')).toBe(1);
+test('should convert I into 1', () => {
+        expect(translateRomanNumber('I')).toBe(1);
 })
 
 test('should convert V into 5', () => {
-	expect(translateRomanNumber('V')).toBe(5);
+        expect(translateRomanNumber('V')).toBe(5);
 })
 
 test('should convert X into 10', () => {
-	expect(translateRomanNumber('X')).toBe(10);
+        expect(translateRomanNumber('X')).toBe(10);
 })
 
 test('should convert D into 50', () => {
-	expect(translateRomanNumber('D')).toBe(50);
+        expect(translateRomanNumber('D')).toBe(50);
 })
