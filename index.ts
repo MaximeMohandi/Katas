1b50881387eef609b22a91c87dd63ad05e7cec86
Maxime Mohandi <mohandi.maxime@gmail.com>
2024-07-08 21:12:18 +0200

    feat(roman): 🎸 sum chained numerals

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 const ROMAN_NUMERALS: { [key: string]: number } = { "I": 1, "V": 5, "X": 10, "D": 50, "C": 100, "M": 1000 };
 
 export function translateRomanNumber(romanNumber: string): number {
+        let standardizedRomanNumber = [...romanNumber.toUpperCase()];
 
-        return ROMAN_NUMERALS[romanNumber.toUpperCase()];
+        let result = 0;
+        standardizedRomanNumber.map((numeral: string) => {
+                result += ROMAN_NUMERALS[numeral]
+        });
+
+        return result;
 }
--- part1.test.ts
+++ part1.test.ts
@@ -1,8 +1,4 @@
-/*Test Cases
- *
- * should convert all 7 letteres to correct number (I:1, V:5, X:10, D:50, C:100, M:1000)
- * 
- */
+
 import { translateRomanNumber } from '.'
 
 describe('should read the 7 roman numerals', () => {
@@ -17,4 +13,12 @@ describe('should read the 7 roman numerals', () => {
 
 test('should not be case sensitive', () => {
         expect(translateRomanNumber('i')).toBe(1);
+})
+
+test('should sum numerals when chained', () => {
+        expect(translateRomanNumber('ii')).toBe(2);
+})
+
+test('should sum numerals when chained more than twice', () => {
+        expect(translateRomanNumber('XXX')).toBe(30);
 })
